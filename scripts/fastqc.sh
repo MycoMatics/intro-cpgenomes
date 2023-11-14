7cf6d768bfb513d6cf5dcd7f03e24e1b905826df
passelma42 <73598059+passelma42@users.noreply.github.com>
2023-11-14 15:20:16 +0100

    Add files via upload

--- scripts/fastqc.sh
+++ scripts/fastqc.sh
@@ -3,8 +3,8 @@
 
 #PBS -N fastqc.$PBS_JOBID                 #  Line 3-7 inform the scheduler about the resources required by this job:
 #PBS -l nodes=1:ppn=all                                    # singe node (nodes=1) all core (ppn=all)
-#PBS -o stdout.$PBS_JOBNAME.$PBS_JOBID                     # redirect sterr stdout to separate files 
-#PBS -e stderr.$PBS_JOBNAME.$PBS_JOBID          
+#PBS -o stdout.$PBS_JOBID                     # redirect sterr stdout to separate files 
+#PBS -e stderr.$PBS_JOBID          
 #PBS -l walltime=01:00:00                                  # run for at most 2 minutes (walltime=00:02:00 max is 72hours)
 #PBS -m abe                                                # send mail when job (a)bort (b)egin (e)nd
 #PBS -M <youremailaddresshere>                             # specify your email address here
@@ -13,11 +13,15 @@
 #Request software
 
 ml FastQC/0.11.9-Java-11
-ml ml MultiQC/1.14-foss-2022
+ml MultiQC/1.14-foss-2022a
 
 #Stage in data: Go to your current working directory and make sure both your data and scrit is there
+
 cd $PBS_O_WORKDIR
 
+#Make output directories
+mkdir fastqc-reports
+
 #Software commands
-fastqc *.fq.gz -o . &&	# run fastqc on all present fq.gz files
+fastqc ./OX0001/*.fq.gz -o ./fastqc-reports &&	# run fastqc on all present fq.gz files
 multiqc .								# run multiqc on all fastqc output && in previous command prevents from starting multiqc early
